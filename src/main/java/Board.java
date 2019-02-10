703ca8a5456e1f8fa5da7d255234b95e20a7d7a9
xinri <h.lay@gop.link>
2019-02-10 21:26:01 +0100

    first implementation

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,7 +1,29 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Board {
+
+  private Cell[][] board;
 
   public void setBoard(String[] boardRepresentation) {
 
+    int length = boardRepresentation[0]
+        .replaceAll(" ", "")
+        .length();
+
+    int height = boardRepresentation.length;
+
+    this.board = new Cell[height][length];
+
+    for (int j = 0; j < height; j++) {
+      for (int i = 0; i < length; i++) {
+        if (boardRepresentation[j].charAt(i*2) == '-') {
+          board[j][i] = Cell.DEAD;
+        } else {
+          board[j][i] = Cell.ALIVE;
+        }
+      }
+    }
   }
 
   public void nextStep() {
@@ -9,10 +31,11 @@ public class Board {
   }
 
   public String[] getBoard() {
-    return new String[] {
-        "- - -",
-        "- - -",
-        "- - -"
-    };
+    return Arrays.stream(board)
+        .map(line ->
+            Arrays.stream(line)
+              .map(cell -> cell == Cell.ALIVE ? "*" : "-")
+              .collect(Collectors.joining(" ")))
+        .toArray(String[]::new);
   }
 }
