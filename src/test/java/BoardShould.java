567610e83e2cccc3f5b6a88a64b59c0ff15abbad
xinri <h.lay@gop.link>
2019-02-10 20:57:17 +0100

    add test with the first rule about surrounding by 4 cell alive

--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -66,4 +66,25 @@ public class BoardShould {
     assertThat(result[1]).isEqualTo("- - -");
     assertThat(result[2]).isEqualTo("- - -");
   }
+
+  @Test
+  public void die_when_there_are_4_cell_around_one() {
+    // given
+    Board board = new Board();
+    board.setBoard(
+        new String[]{
+            "- * -",
+            "* * *",
+            "- * -"
+        });
+
+    // when
+    board.nextStep();
+    String[] result = board.getBoard();
+
+    // then
+    assertThat(result[0]).isEqualTo("* * *");
+    assertThat(result[1]).isEqualTo("* - *");
+    assertThat(result[2]).isEqualTo("* * *");
+  }
 }
